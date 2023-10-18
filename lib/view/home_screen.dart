bc50184684cab5b2c4b554cb0e39e63adc94ecdf
abiodunosagie <abiodunosagie8@gmail.com>
2023-10-18 14:44:06 +0100

    weather services import

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -2,9 +2,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
-import 'package:talk_podcast/services/weather_services.dart';
-
 import '../model/weather_model.dart';
+import '../services/weather_services.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
